135a39afbdc1c3922364d0f7dd5caa98adbc55b2
Sanelsss <sanel_omanovic@hotmail.com>
2021-10-07 20:32:33 +0200

    Updates

---
+++ precache-manifest.a0d0068dd5bad0959bdf9aa884dfb582.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd3e47368fc0cf2e32497bc8a5927e7",
+    "url": "/storecetiri/index.html"
+  },
+  {
+    "revision": "f01e5272e9e8c6f41ec2",
+    "url": "/storecetiri/static/css/2.039bb34a.chunk.css"
+  },
+  {
+    "revision": "30d4b7449c8f0ee228b6",
+    "url": "/storecetiri/static/css/main.69d269cf.chunk.css"
+  },
+  {
+    "revision": "f01e5272e9e8c6f41ec2",
+    "url": "/storecetiri/static/js/2.f93763b5.chunk.js"
+  },
+  {
+    "revision": "75fc5a0ff22eb52fcd50272201589016",
+    "url": "/storecetiri/static/js/2.f93763b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d4b7449c8f0ee228b6",
+    "url": "/storecetiri/static/js/main.4a099306.chunk.js"
+  },
+  {
+    "revision": "f905e9fbf6d5d189d72d",
+    "url": "/storecetiri/static/js/runtime-main.0d9e1bbd.js"
+  },
+  {
+    "revision": "882a211ad0bae36b9c11715c9291f36b",
+    "url": "/storecetiri/static/media/hero.882a211a.svg"
+  }
+]);
